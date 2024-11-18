583702c6c7545611dbc77b03a32a5bb671b01668
Artem Cherednychenko <artem.cherednychenko2817@gmail.com>
2024-11-18 17:35:13 +0100

    Second

--- Art3/Program.cs
+++ Art3/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Artem!");
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+
+{
+    Console.WriteLine("Введіть номер дня тижня (1-7):"); 
+    string input = Console.ReadLine(); 
+
+
+        if (input != null && input.All(char.IsDigit))
+
+        { 
+        int day_number = int.Parse(input); 
+        switch (day_number) 
+        { 
+            case 1: 
+                Console.WriteLine("Це понеділок."); 
+                break; 
+
+            case 2: 
+                Console.WriteLine("Це вівторок."); 
+                break; 
+            
+            case 3: 
+                Console.WriteLine("Це середа."); 
+                break; 
+
+            case 4:
+                Console.WriteLine("Це четвер."); 
+                break; 
+            
+            case 5: 
+                Console.WriteLine("Це п'ятниця."); 
+                break; 
+
+            case 6: 
+                Console.WriteLine("Це субота."); 
+                break; 
+            
+            case 7:
+                Console.WriteLine("Це неділя."); 
+                break; 
+            
+            default: 
+                Console.WriteLine("Невірний номер дня.");
+                break; 
+        } 
+        }
+
+        else 
+        { 
+            Console.WriteLine("Невірний номер дня."); 
+        } 
+} 
