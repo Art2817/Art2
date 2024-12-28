8d48437eccfce5eb7bef64be5a2a3fd2e3e25b14
Artem Cherednychenko <artem.cherednychenko2817@gmail.com>
2024-12-28 12:03:15 +0100

    14 commit

---
+++ Dz15/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+namespace Dz15
+{
+    public abstract class Product
+    {
+        public string Name 
+        { 
+            get; 
+        }
+
+
+        protected decimal BasePrice 
+        { 
+            get; 
+        }
+
+        protected Product(string name, decimal basePrice)
+        {
+            Name = name;
+            BasePrice = basePrice;
+        }
+
+        public abstract decimal Price();
+
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {BasePrice}";
+        }
+    }
+
+    public class Carrot : Product
+    {
+        public Carrot(decimal basePrice) : base("Carrot", basePrice) 
+        { 
+        }
+
+        public  override decimal Price() => BasePrice;
+    }
+
+    public class Potato : Product
+    {
+        public int Count 
+        { 
+            get; 
+        }
+
+        public Potato(decimal basePrice, int count) : base("Potato", basePrice)
+        {
+            Count = count;
+        }
+
+        public override decimal Price() => BasePrice * Count;
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Count: {Count}, Total price: {Price()}";
+        }
+    }
+
+
+    public class Cucumber : Product
+    {
+        public int Count 
+        { 
+            get; 
+        }
+
+        public Cucumber(decimal basePrice, int count) : base("Cucumber", basePrice)
+        {
+            Count = count;
+        }
+
+        public override decimal Price() => BasePrice * Count;
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Count: {Count}, Total price: {Price()}";
+        }
+    }
+
+    public class Tomato : Product
+    {
+        public Tomato(decimal basePrice) : base("Tomato", basePrice) 
+        { 
+        }
+
+        public override decimal Price() => BasePrice;
+    }
+
+    public class VegetableShop
+    {
+        private readonly List<Product> Products = new();
+
+        public void Add(IEnumerable<Product> products)
+        {
+            Products.AddRange(products);
+        }
+
+        public void Info()
+        {
+            decimal totalPrice = 0;
+
+            foreach (var product in Products)
+            {
+                Console.WriteLine(product);
+                totalPrice += product.Price();
+            }
+
+            Console.WriteLine($"Total products price: {totalPrice}");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var products = new List<Product>
+        {
+            new Carrot(15),
+            new Potato(20, 4),
+            new Cucumber(14, 2)
+        };
+
+            var shop = new VegetableShop();
+            shop.Add(products);
+
+            shop.Info();
+        }
+    }
+}
---
+++ Dz16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Dz16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            try
+            {
+
+                Console.Write("Введіть шлях до вихіідного файлу: ");
+                string sourcePath = Console.ReadLine();
+
+
+                Console.Write("Введіть шлях до файлу в який потрібно скопіюват:");
+                string destinationPath = Console.ReadLine();
+
+                if (!File.Exists(sourcePath))
+                {
+                    Console.WriteLine("Вихідний файл не знайдено");
+                    return;
+                }
+
+                File.Copy(sourcePath, destinationPath);
+
+                Console.WriteLine("Файл успішно скопійовано ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Сталася помилка: {ex.Message}");
+            }
+        }
+    }
+}
